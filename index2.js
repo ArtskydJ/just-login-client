450b37dc0f2bd8193f263a458c246761e835ab34
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-04 12:38:33 -0500

    updated index2

--- index2.js
+++ index2.js
@@ -1,27 +1,14 @@
-var dnode = require('dnode')
-//var domready = require('domready');
-var shoe = require('shoe');
-var stream = shoe('/dnode')
-var events = require('events')
-
-var api = {
-	beginAuthentication: function() {},
-	isAuthenticated: function(cb) {cb(null, false)},
-	unauthenticate: function() {},
-}
-var d = dnode() //no listening, and do not run d.end()!
-d.on('remote', function (tempApi) {
-	api = tempApi //the api at the module's scope
-	console.log("yo found it!", api)
-	glob = api //allows me to run these functions in the browser console
-})
-d.pipe(stream).pipe(d);
-
-function createSession(cb) { //cb(err, api, session)
-	//must get actual session id if existing, instead of hardcoded 13 below
-	api.continueExistingSession(13, function(err, fullApi, sessionId) {
+function createSession(api, cb) { //cb(err, api, session)
+	var existing = localStorage.getItem("justLoginSessionId")
+	api.continueExistingSession(existing, function (err, fullApi, sessionId) {
 		if (err && err.invalidSessionId) { //bad session id attempt
-			api.createNewSession(cb)
+			api.createNewSession(function (err, newFullApi, newSessionId){
+				localStorage.setItem("justLoginSessionId", newSessionId)
+				if (err)
+					cb(err)
+				else
+					cb(null, newFullApi, newSessionId)
+			})
 		}
 		else if (err)
 			cb(err)
@@ -30,40 +17,4 @@ function createSession(cb) { //cb(err, api, session)
 	})
 }
 
-var onLoginTimer = null
-var emitter = new events.EventEmitter()
-function onLogin() {
-	if (!onLoginTimer) {
-		onLoginTimer = setInterval(function() {
-			api.isAuthenticated(function(err, addr) {
-				if (!err && addr) {
-					clearInterval(onLoginTimer)
-					emitter.emit('login')
-				}
-			})
-		}, 1000)
-	}
-	return emitter
-}
-
-module.exports = {
-	createSession: createSession,
-	onLogin: onLogin
-}
-
-/*
-var events = require('events')
-
-	var emitter = new events.EventEmitter()
-	var loggedIn, prevLoggedIn
-	emitter.beginAuthentication = jlc.beginAuthentication.bind(jlc, sessionId)
-	emitter.isAuthenticated =     jlc.isAuthenticated.bind(jlc, sessionId)
-	emitter.unauthenticate =      jlc.unauthenticate.bind(jlc, sessionId)
-	prevLoggedIn = emitter.isAuthenticated()
-	setInterval(function() {
-		loggedIn = emitter.isAuthenticated()
-		if (loggedIn != prevLoggedIn) {
-			emitter.emit('log'+loggedIn?'in':'out')
-		}
-	}, 1000)
-*/
+module.exports = createSession
