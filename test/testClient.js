445610e84423e8497a2dd53a4858a4fef9d4d428
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-08 16:08:32 -0500

    Updated testClient.js

--- test/testClient.js
+++ test/testClient.js
@@ -1,27 +1,51 @@
 var test = require('tap').test
 var createSession = require('../index.js')
 
-var fakeSessionId = "fakeSessionId"
-var fakeApi
+if (typeof localStorage === "undefined" || localStorage === null) {
+	console.log("replacing localStorage")
+	var LocalStorage = require('node-localstorage').LocalStorage
+	localStorage = new LocalStorage('./scratch')
+}
 
-//unfortunately must create this
-localStorage = require('mock-dom-storage')
+var jlsid = "justLoginSessionId" //key
+var fakeSessionId = "fakeSessionId" //value
+var fakeArgApi = "Joseph" //this would usually be an object
+var newSessionId = "newSessionId"
+var fakeApi = {
+	continueExistingSession: function (get, cb) {
+		if (get == fakeSessionId) {
+			cb(null, fakeArgApi, fakeSessionId)
+		} else if (get == null) {
+			var err = new Error("u haz error")
+			err.invalidSessionId = true
+			console.log("u haz errorz!")
+			cb(err)
+		} else {
+			cb(null)
+		}
+	},
+	createNewSession: function (cb) {
+		cb(null, fakeArgApi, newSessionId)
+	}
+}
 
 test('test createSession', function (t) {
-
-	localStorage.setItem("justLoginSessionId", fakeSessionId) //set the session id
-
+	t.plan(9)
+	localStorage.setItem(jlsid, fakeSessionId) //set the session id
+	t.equal(localStorage.getItem(jlsid), fakeSessionId, "localStorage works")
 	createSession(fakeApi, function (err, api, session) {
 		t.notOk(err, "no error")
-		t.equal(session, fakeSessionId, "Session Id in localStorage") //must retrieve session id
-		t.notEqual(fakeApi, api, "these must be very different")
+		t.equal(session, fakeSessionId, "sessionId must be old") //must retrieve session id
+		t.notEqual(session, newSessionId, "sessionId must not be new")
+		t.equal(fakeArgApi, api, "these must be the same")
 
 		localStorage.removeItem("justLoginSessionId") //delete the session id
 		createSession(fakeApi, function (err, api, session) {
 			t.notOk(err, "no error")
-			t.notEqual(session, fakeSessionId, "Session Id is new") //creates session id
-			t.notEqual(fakeApi, api, "these must be very different")
+			t.notEqual(session, fakeSessionId, "sessionId must not be old") //creates session id
+			t.equal(session, newSessionId, "sessionId must be new")
+			t.equal(fakeArgApi, api, "these must be the same")
+			t.end()
 		})
-
 	})
 })
