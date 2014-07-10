9942b3f1b39968ccd5a965b6aeb75e73e5918fad
Joseph Dykstra <josephdykstra@gmail.com>
2014-07-10 16:34:25 -0500

    Added error properties

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ function createSession(api, cb) { //cb(err, api, session)
 	var existing = localStorage.getItem("justLoginSessionId")
 	api.continueExistingSession(existing, function (err, fullApi, sessionId) {
 		if (err && err.invalidSessionId) { //bad session id attempt
-			api.createNewSession(function (err, newFullApi, newSessionId){
+			api.createNewSession(function (err2, newFullApi, newSessionId){
 				localStorage.setItem("justLoginSessionId", newSessionId)
-				if (err)
-					cb(err)
-				else
+				if (err2) {
+					err2.apiCns = true
+					cb(err2)
+				} else {
 					cb(null, newFullApi, newSessionId)
+				}
 			})
 		}
 		else if (err)
+			err.apiCes = true
 			cb(err)
 		else
 			cb(null, fullApi, sessionId)
