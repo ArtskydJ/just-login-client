3b2a0407891fea9176a7f1e45fc180651b10e641
Josh Duff <me@JoshDuff.com>
2014-07-31 21:35:40 -0500

    Now emitting on every authentication

--- createSession.js
+++ createSession.js
@@ -1,6 +1,28 @@
+function overwriteBeginAuthentication(fullApi, beginWatchingForAuthentication) {
+	var exposedApi = Object.create(fullApi)
+	exposedApi.beginAuthentication = function beginAuthentication(emailAddress, cb) {
+		beginWatchingForAuthentication(fullApi)
+		fullApi.beginAuthentication(emailAddress, cb)
+	}
+	return exposedApi
+}
+
 function createSession(api, emitter, cb) { //cb(err, api, session)
 	var existing = localStorage.getItem("justLoginSessionId")
+
+	var beginWatchingForAuthentication = function beginWatchingForAuthentication(fullApi) {
+		var timer = setInterval(function() {
+			fullApi.isAuthenticated(function(err, name) {
+				if (!err && name) {
+					emitter.emit('authenticated', name)
+					clearInterval(timer)
+				}
+			})
+		}, 2000)
+	}
+
 	api.continueExistingSession(existing, function (err, fullApi, sessionId) {
+
 		if (err) { //bad session id attempt
 			api.createNewSession(function (err, fullApi, sessionId) {
 				if (err) {
@@ -11,23 +33,16 @@ function createSession(api, emitter, cb) { //cb(err, api, session)
 						emitter.emit('new session', sessionId)
 					})
 
-					var timer = setInterval(function() {
-						fullApi.isAuthenticated(function(err, name) {
-							if (!err && name) {
-								emitter.emit('authenticated', name)
-								clearInterval(timer)
-							}
-						})
-					}, 2000)
+					beginWatchingForAuthentication(fullApi)
 
-					cb(null, fullApi, sessionId)
+					cb(null, overwriteBeginAuthentication(fullApi, beginWatchingForAuthentication), sessionId)
 				}
 			})
 		} else {
 			process.nextTick(function() {
 				emitter.emit('continue session', sessionId)
 			})
-			cb(null, fullApi, sessionId)
+			cb(null, overwriteBeginAuthentication(fullApi, beginWatchingForAuthentication), sessionId)
 		}
 	})
 }
