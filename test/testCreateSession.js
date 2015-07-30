808dea8262eee0a834bda7d2ef95e5fd3bc2bccf
Joseph Dykstra <josephdykstra@gmail.com>
2015-07-30 08:42:57 -0500

    fixed tests

--- test/testCreateSession.js
+++ test/testCreateSession.js
@@ -1,85 +1,88 @@
 var test = require('tap').test
 var EventEmitter = require('events').EventEmitter
-var establishSession = require('../createSession.js')
+var acquireSession = require('../acquireSession.js')
+var mockDomStorage = require('mock-dom-storage')
 
-if (process) { //if running in node
-	var LocalStorage = require('node-localstorage').LocalStorage
-	localStorage = new LocalStorage('./fakeLocalStorage') //var is not supposed to be here :(
+if (typeof localStorage === 'undefined') { //if running in node
+	global.localStorage = mockDomStorage()
 }
+localStorage.clear()
 
-var localStorageKey = "justLoginSessionId" //key
-var fakeExistingSessionId = "fake-existing-session-identification" //value
-var fakeNewSessionId = "newSessionId"
-var fakeEmailAddress = "ex@mp.le"
+var localStorageKey = 'justLoginSessionId' //key
+var fakeExistingSessionId = 'fake-existing-session-identification' //value
+var fakeNewSessionId = 'newSessionId'
+var fakeEmailAddress = 'ex@mp.le'
 var fakeArgApi = {
 	isAuthenticated: function (cb) {
 		cb(null, fakeEmailAddress)
 	}
 }
-var fakeApi = {
-	continueSession: function (get, cb) {
-		if (get === fakeExistingSessionId) {
-			cb(null, fakeArgApi, fakeExistingSessionId)
-		} else {
-			cb(new Error("u haz error"))
+function fakeApi(expectId) {
+	return {
+		getFullApi: function (id, cb) {
+			if (id === expectId) {
+				cb(null, fakeArgApi)
+			} else {
+				cb(new Error('u haz error'))
+			}
+		},
+		createSession: function (cb) {
+			setTimeout(cb, 0, null, fakeNewSessionId)
 		}
-	},
-	createSession: function (cb) {
-		cb(null, fakeArgApi, fakeNewSessionId)
 	}
 }
 
 
-test('create a new session with establishSession', function (t) {
+test('create a new session with acquireSession', function (t) {
 	t.plan(8)
 	var emitter = new EventEmitter()
-	
-	establishSession(fakeApi, emitter, function (err, api, session) {
-		t.notOk(err, "no error")
-		t.notEqual(session, fakeExistingSessionId, "sessionId must not be old") //creates session id
-		t.equal(session, fakeNewSessionId, "sessionId must be new")
-		t.equal(typeof api.isAuthenticated, "function", "api has 'isAuthenticated' function")
-		t.equal(typeof api.beginAuthentication, "function", "api has beginAuthentication() function [added by overwriteBeginAuthentication()]")
+
+	acquireSession(fakeApi(fakeNewSessionId), emitter, function (err, api, session) {
+		t.notOk(err, err ? err.message : 'no error')
+		t.notEqual(session, fakeExistingSessionId, 'sessionId must not be old') //creates session id
+		t.equal(session, fakeNewSessionId, 'sessionId must be new')
+		t.equal(api && typeof api.isAuthenticated, 'function', 'api has isAuthenticated() function')
+		t.equal(api && typeof api.beginAuthentication, 'function', 'api has beginAuthentication() function [added by overwriteBeginAuthentication()]')
 		setTimeout(t.end.bind(t), 2010) //must wait for authenticated event to be called
 	})
 
 	emitter.on('session', function (data) {
-		t.notOk(data.continued, "created a NEW session")
-		t.ok(data.sessionId, "has sessionId property")
-		t.equal(data.sessionId, fakeNewSessionId, "created a session id")
+		t.notOk(data.continued, 'created a NEW session')
+		t.ok(data.sessionId, 'has sessionId property')
+		t.equal(data.sessionId, fakeNewSessionId, 'created a session id')
 	})
 
 	emitter.on('authenticated', function (whom) {
-		t.ok(whom, "got authenticated")
-		t.equal(whom, fakeEmailAddress, "correct email (new)")
+		t.ok(whom, 'got authenticated')
+		t.equal(whom, fakeEmailAddress, 'correct email (new)')
 	})
 })
 
-test('continue an existing session with establishSession', function (t) {
+test('continue an existing session with acquireSession', function (t) {
 	t.plan(7)
 
 	localStorage.setItem(localStorageKey, fakeExistingSessionId) //set the session id
-	t.equal(localStorage.getItem(localStorageKey), fakeExistingSessionId, "localStorage works")
+	t.equal(localStorage.getItem(localStorageKey), fakeExistingSessionId, 'localStorage works')
 
 	var emitter = new EventEmitter()
 
-	localStorage.setItem("keepDatabaseFromBecomingEmpty", fakeExistingSessionId)
+	localStorage.setItem('keepDatabaseFromBecomingEmpty', fakeExistingSessionId)
 
-	establishSession(fakeApi, emitter, function (err, api, session) {
-		t.notOk(err, "no error")
-		t.equal(fakeExistingSessionId, session, "sessionId must be old") //must retrieve session id
-		t.notEqual(fakeNewSessionId, session, "sessionId must not be new")
-		t.notEqual(fakeArgApi, api, "these must be the different because of overwriteBeginAuthentication()") //changed from .equal to .deepEqual 20140802
+	acquireSession(fakeApi(fakeExistingSessionId), emitter, function (err, api, session) {
+		t.notOk(err, 'no error')
+		t.equal(fakeExistingSessionId, session, 'sessionId must be old') //must retrieve session id
+		t.notEqual(fakeNewSessionId, session, 'sessionId must not be new')
+		t.notEqual(fakeArgApi, api, 'these must be the different because of overwriteBeginAuthentication()') //changed from .equal to .deepEqual 20140802
 
 		localStorage.removeItem(localStorageKey) //delete the session id
 		setTimeout(t.end.bind(t), 2010) //must wait for authenticated event to be called
 	})
 
 	emitter.on('session', function (data) {
-		t.ok(data.continued, "continued an existing session")
-		t.equal(data.sessionId, fakeExistingSessionId, "got correct id")
+		t.ok(data.continued, 'continued an existing session')
+		t.equal(data.sessionId, fakeExistingSessionId, 'got correct id')
 	})
 	emitter.on('authenticated', function (whom) {
-		t.fail("not supposed to get this event here...")
+		t.fail('not supposed to get this event here...')
 	})
 })
